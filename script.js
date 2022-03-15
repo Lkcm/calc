cbfb14004cb296772ae5593d277b131094a64e7f
Lucas Kroger <lucasmkroger12@gmail.com>
2022-03-15 18:28:24 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 console.log("Ola mundo")
 
+// let r1 = 1
 
-    let n1 = document.getElementsByClassName("n1")[0];
-    let n2 = document.getElementsByClassName("n2")[0];
-    let numero1 = parseInt(n1.textContent);
-    let numero2 = parseInt(n2.textContent);
-    let soma = numero1+numero2;
+// let r2 = 2
+
+// let inputN1 = document.getElementById("n1");
+
+// let inputN2 = document.getElementById("n2");
+
+// function calculator() {
+//     console.log("Calculand...");
+
+//     let = n1 
+
+}
+
+// function inputN1(input) {
+//     let n1 = document.getElementById("n1");
+//     let r1 = n1.innerText = input.value;
+//     console.log(r1)
+//     return r1
+// }
+
+// function inputN2(input) {
+//     let n2 = document.getElementById("n2");
+//     let r2 = n2.innerText = input.value;
+//     console.log(r2)
+//     return r2
+// }
+
+    // let num1 = document.getElementsByClassName("n1")[0];
+    // let num2 = document.getElementsByClassName("n2")[0];
+    // let numero1 = parseInt(num1.textContent);
+    // let numero2 = parseInt(num2.textContent);
+    // let soma = numero1+numero2;
     
-    document.getElementById("soma").innerHTML = (soma)
+    // document.getElementById("soma").innerHTML = (soma)
     // document.getElementById("soma").innerHTML = `${soma} `
 
     
